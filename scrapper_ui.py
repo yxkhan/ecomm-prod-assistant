fd6f6b95e3e68a33a46748f243d64ec39868e860
sunnysavita10 <engsunny3365@gmail.com>
2025-08-31 16:13:16 +0530

    code is updated

--- scrapper_ui.py
+++ scrapper_ui.py
@@ -1,8 +1,10 @@
+
 import streamlit as st
-#from data_scrapper.scrape_data import scrape_flipkart_products, save_to_csv
-#from data_ingestion.ingestion_pipeline import DataIngestion
+from prod_assistant.etl.data_scrapper import FlipkartScraper
+from prod_assistant.etl.data_ingestion import DataIngestion
 import os
 
+flipkart_scraper = FlipkartScraper()
 output_path = "data/product_reviews.csv"
 st.title("📦 Product Review Scraper")
 
@@ -38,20 +40,17 @@ if st.button("🚀 Start Scraping"):
         final_data = []
         for query in product_inputs:
             st.write(f"🔍 Searching for: {query}")
-            results = scrape_flipkart_products(query, max_products=max_products, review_count=review_count)
+            results = flipkart_scraper.scrape_flipkart_products(query, max_products=max_products, review_count=review_count)
             final_data.extend(results)
 
         unique_products = {}
         for row in final_data:
             if row[1] not in unique_products:
                 unique_products[row[1]] = row
-        # final_data = list(unique_products.values())
 
-        # save_to_csv(final_data, output_path)
-        
         final_data = list(unique_products.values())
-        st.session_state["scraped_data"] = final_data  # ✅ store in session
-        save_to_csv(final_data, output_path)
+        st.session_state["scraped_data"] = final_data  # store in session
+        flipkart_scraper.save_to_csv(final_data, output_path)
         st.success("✅ Data saved to `data/product_reviews.csv`")
         st.download_button("📥 Download CSV", data=open(output_path, "rb"), file_name="product_reviews.csv")
 
