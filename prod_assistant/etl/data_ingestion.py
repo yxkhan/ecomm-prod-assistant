d45c526a2d6aab981779e4e0b77ec74bae237a66
sunnysavita10 <engsunny3365@gmail.com>
2025-08-31 15:53:11 +0530

    clean skeleton is updated

--- prod_assistant/etl/data_ingestion.py
+++ prod_assistant/etl/data_ingestion.py
@@ -12,23 +12,37 @@ class DataIngestion:
     Class to handle data transformation and ingestion into AstraDB vector store.
     """
     def __init__(self):
+        """_summary_
+        """
         pass
     
     def _load_env_variables(self):
+        """Load environment variables from .env file.
+        """
         pass
         
     def _get_csv_path(self):
+        """Get the path to the CSV file.
+        """
         pass
     
     def _load_csv(self):
+        """Load the CSV file into a DataFrame.
+        """
         pass
     
     def transform_data(self):
+        """Transform the data for ingestion.
+        """
         pass
     
     def store_in_vector_db(self):
+        """Store the transformed data in the vector database.
+        """
         pass
     
     def run_pipeline(self):
+        """Run the entire data ingestion pipeline.
+        """
         pass
     
--- prod_assistant/etl/data_scrapper.py
+++ prod_assistant/etl/data_scrapper.py
@@ -14,10 +14,15 @@ class FlipkartScraper:
         os.makedirs(self.output_dir, exist_ok=True)
 
     def get_top_reviews(self,product_url,count=2):
+        """Get the top reviews for a product.
+        """
         pass
     
     def scrape_flipkart_products(self, query, max_products=1, review_count=2):
+        """Scrape Flipkart products based on a search query.
+        """
         pass
     
     def save_to_csv(self, data, filename="product_reviews.csv"):
+        """Save the scraped product reviews to a CSV file."""
         pass
