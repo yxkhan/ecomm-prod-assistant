f8d15cff3b541208f8977a7bc569a04bfa950b6f
sunnysavita10 <engsunny3365@gmail.com>
2025-09-13 16:47:48 +0530

    changes updated in main.py file

--- prod_assistant/router/main.py
+++ prod_assistant/router/main.py
@@ -6,8 +6,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from langchain_core.messages import HumanMessage
-
-from workflow.workflow_class import AgenticRAG
+from workflow.agentic_rag_workflow import AgenticRAG
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
