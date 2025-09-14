c74a4d7e8b320aa45b378f4d13a223e14831baf4
sunnysavita10 <engsunny3365@gmail.com>
2025-09-14 16:07:50 +0530

    advance retrieval implemented

--- prod_assistant/retriever/retrieval.py
+++ prod_assistant/retriever/retrieval.py
@@ -8,6 +8,9 @@ from dotenv import load_dotenv
 import sys
 from pathlib import Path
 
+from langchain.retrievers.document_compressors import LLMChainFilter
+from langchain.retrievers import ContextualCompressionRetriever
+
 # Add the project root to the Python path for direct script execution
 project_root = Path(__file__).resolve().parents[2]
 sys.path.insert(0, str(project_root))
@@ -54,9 +57,26 @@ class Retriever:
                 )
         if not self.retriever:
             top_k = self.config["retriever"]["top_k"] if "retriever" in self.config else 3
-            retriever=self.vstore.as_retriever(search_kwargs={"k": top_k})
+            
+            mmr_retriever=self.vstore.as_retriever(
+                search_type="mmr",
+                search_kwargs={"k": top_k,
+                                "fetch_k": 20,
+                                "lambda_mult": 0.7,
+                                "score_threshold": 0.3
+                               })
             print("Retriever loaded successfully.")
-            return retriever
+            
+            llm = self.model_loader.load_llm()
+            
+            compressor=LLMChainFilter.from_llm(llm)
+            
+            self.retriever = ContextualCompressionRetriever(
+                base_compressor=compressor, 
+                base_retriever=mmr_retriever
+            )
+            
+            return self.retriever
             
     def call_retriever(self,query):
         """_summary_
