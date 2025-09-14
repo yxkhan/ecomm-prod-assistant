ad56f694e3d24a063a72867fd020a53da2d7df83
sunnysavita10 <engsunny3365@gmail.com>
2025-09-14 17:10:21 +0530

    workflow modified for the testing

--- prod_assistant/workflow/normal_generation_workflow.py
+++ prod_assistant/workflow/normal_generation_workflow.py
@@ -30,9 +30,13 @@ def format_docs(docs) -> str:
     return "\n\n---\n\n".join(formatted_chunks)
 
 
-def build_chain():
+def build_chain(query):
     """Build the RAG pipeline chain with retriever, prompt, LLM, and parser."""
     retriever = retriever_obj.load_retriever()
+    retrieved_docs=retriever.invoke(query)
+    
+    retrieved_contexts = [format_docs(doc) for doc in retrieved_docs]
+    
     llm = model_loader.load_llm()
     prompt = ChatPromptTemplate.from_template(
         PROMPT_REGISTRY[PromptType.PRODUCT_BOT].template
@@ -44,12 +48,12 @@ def build_chain():
         | llm
         | StrOutputParser()
     )
-    return chain
+    return chain,retrieved_contexts
 
 
-def invoke_chain(query: str, debug: bool = False) -> str:
+def invoke_chain(query: str, debug: bool = False):
     """Run the chain with a user query."""
-    chain = build_chain()
+    chain,retrieved_contexts = build_chain(query)
 
     if debug:
         # For debugging: show docs retrieved before passing to LLM
@@ -58,42 +62,37 @@ def invoke_chain(query: str, debug: bool = False) -> str:
         print(format_docs(docs))
         print("\n---\n")
 
-    return chain.invoke(query)
+    response = chain.invoke(query)
+    
+    return retrieved_contexts,response
 
-# if __name__ == "__main__":
-#     try:
-#         answer = invoke_chain("can you tell me the price of the iPhone 15?")
-#         print("\n Assistant Answer:\n", answer)
-#     except Exception as e:
-#         import traceback
-#         print("Exception occurred:", str(e))
-#         traceback.print_exc()
 
 if __name__=='__main__':
     user_query = "Can you suggest good budget iPhone under 1,00,000 INR?"
+     
+    #retriever_obj = Retriever()
     
-    retriever_obj = Retriever()
+    #retrieved_docs = retriever_obj.call_retriever(user_query)
     
-    retrieved_docs = retriever_obj.call_retriever(user_query)
+    # def _format_docs(docs) -> str:
+    #     if not docs:
+    #         return "No relevant documents found."
+    #     formatted_chunks = []
+    #     for d in docs:
+    #         print(d)
+    #         meta = d.metadata or {}
+    #         formatted = (
+    #             f"Title: {meta.get('product_title', 'N/A')}\n"
+    #             f"Price: {meta.get('price', 'N/A')}\n"
+    #             f"Rating: {meta.get('rating', 'N/A')}\n"
+    #             f"Reviews:\n{d.page_content.strip()}"
+    #         )
+    #         formatted_chunks.append(formatted)
+    #     return "\n\n---\n\n".join(formatted_chunks)
     
-    def _format_docs(docs) -> str:
-        if not docs:
-            return "No relevant documents found."
-        formatted_chunks = []
-        for d in docs:
-            meta = d.metadata or {}
-            formatted = (
-                f"Title: {meta.get('product_title', 'N/A')}\n"
-                f"Price: {meta.get('price', 'N/A')}\n"
-                f"Rating: {meta.get('rating', 'N/A')}\n"
-                f"Reviews:\n{d.page_content.strip()}"
-            )
-            formatted_chunks.append(formatted)
-        return "\n\n---\n\n".join(formatted_chunks)
+    # retrieved_contexts = [_format_docs(doc) for doc in retrieved_docs]
     
-    retrieved_contexts = [_format_docs(doc) for doc in retrieved_docs]
-    
-    response = invoke_chain(user_query)
+    retrieved_contexts,response = invoke_chain(user_query)
     
     #this is not an actual output this have been written to test the pipeline
     #response="iphone 16 plus, iphone 16, iphone 15 are best phones under 1,00,000 INR."
@@ -104,3 +103,14 @@ if __name__=='__main__':
     print("\n--- Evaluation Metrics ---")
     print("Context Precision Score:", context_score)
     print("Response Relevancy Score:", relevancy_score)
+    
+    
+    
+# if __name__ == "__main__":
+#     try:
+#         answer = invoke_chain("can you tell me the price of the iPhone 15?")
+#         print("\n Assistant Answer:\n", answer)
+#     except Exception as e:
+#         import traceback
+#         print("Exception occurred:", str(e))
+#         traceback.print_exc()
